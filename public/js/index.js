10816cce342207a01adfe11a135cb0797f107fa7
Derrick Adkins <derrick.l.adkins@gmail.com>
2023-11-30 08:06:06 -0500

    Update clock-in functionality and add logout button

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,4 @@
+import { get } from 'http';
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js';
 import { getFirestore, collection, doc, getDoc } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js';
@@ -41,10 +42,19 @@ document.getElementById('googleSignIn').addEventListener('click', () => {
         });
 });
 
+document.getElementById('logout').addEventListener('click', () => {
+    auth.signOut().then(() => {
+        console.log('Signed out successfully!');
+    }).catch((error) => {
+        console.log(error);
+    });
+});
+
 async function applyUser(user) {
     if (user) {
-        // User is signed in
-        var uid = user.uid;
+        // Show logout button
+        document.getElementById('logout').style.display = 'block';
+        document.getElementById('googleSignIn').style.display = 'none';
 
         // Get admins document from users collection
         const adminsRef = doc(db, 'users', 'admins');
@@ -57,10 +67,14 @@ async function applyUser(user) {
                     // Show admin div
                     document.getElementById('admin').style.display = 'block';
                     document.getElementById('user').style.display = 'none';
+
+                    getAllSubmissions();
                 } else {
                     // Show user div
                     document.getElementById('user').style.display = 'block';
                     document.getElementById('admin').style.display = 'none';
+
+                    getUserSumbissions(user);
                 }
             } else {
                 console.log("No such document!");
@@ -69,6 +83,10 @@ async function applyUser(user) {
             console.log("Error getting document:", error);
         }
     } else {
+        // Show login button
+        document.getElementById('googleSignIn').style.display = 'block';
+        document.getElementById('logout').style.display = 'none';
+
         // Hide user and admin divs
         document.getElementById('user').style.display = 'none';
         document.getElementById('admin').style.display = 'none';
@@ -77,15 +95,36 @@ async function applyUser(user) {
 
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, showError);
+        navigator.geolocation.getCurrentPosition(clockIn, showError);
     } else { 
         document.getElementById("location").innerHTML = "Geolocation is not supported by this browser.";
     }
 }
 
-function showPosition(position) {
-    document.getElementById("location").innerHTML = "Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude;
+async function clockIn(position) {
+    // submit to firestore
+    const user = auth.currentUser;
+    const timestamp = Date.now();
+    const geopoint = new firebase.firestore.GeoPoint(position.coords.latitude, position.coords.longitude);
+    const docRef = doc(db, 'submissions', user.uid);
+
+    const newSubmission = {
+        time: timestamp,
+        place: geopoint
+    };
+
+    await updateDoc(docRef, {
+        submissions: firebase.firestore.FieldValue.arrayUnion(newSubmission)
+    }).then(() => {
+        console.log('Document successfully updated!');
+    }).catch((error) => {
+        console.error('Error updating document: ', error);
+    });
+
+    // show success message
+    document.getElementById("response").innerHTML = "Clock in successful!";
+    // refresh clock ins
+    getUserSumbissions(user);
 }
 
 function showError(error) {
@@ -103,4 +142,33 @@ function showError(error) {
             document.getElementById("location").innerHTML = "An unknown error occurred."
             break;
     }
+}
+
+function getUserSumbissions(user){
+    const docRef = doc(db, 'submissions', user.uid);
+    getDoc(docRef).then((doc) => {
+        if (doc.exists()) {
+            console.log("Document data:", doc.data());
+            document.getElementById('clockIns').innerHTML = doc.data();
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+            document.getElementById('clockIns').innerHTML = 'No submissions found';
+        }
+    }).catch((error) => {
+        console.log("Error getting document:", error);
+    });
+}
+
+function getAllSubmissions(){
+    // get every document from the submissions collection
+    const submissionsRef = collection(db, 'submissions');
+    const querySnapshot = getDocs(submissionsRef);
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+        // output to submissions paragraph
+        document.getElementById('submissions').innerHTML += `${doc.id} => ${doc.data()}`;
+        // add a line break
+        document.getElementById('submissions').innerHTML += '<br>';
+    });
 }
